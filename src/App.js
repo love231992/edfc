02d284a6162dde294015493ab48c6616140bf04c
iamlove <love231992@gmail.com>
2020-06-23 19:51:27 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import pic from "./sunwithtrack.jpg"
+import axios from 'axios';
+
 
 function App() {
+  const getPosts = () => {
+    axios.get('https://cors-anywhere.herokuapp.com/http://ballast-data.herokuapp.com/')
+         .then((posts) => setPosts(posts.data))
+         .catch((error) => console.log(error));
+  };
+  
+  const [posts, setPosts] = React.useState([]);
+  console.log((posts))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={pic} alt="train"></img>
+      <h1 className="heading">EDFC CP-303</h1>
+      <div className="btn" onClick={ () => getPosts()}>Press Me</div>
+      <div className="data">
+        <li>{posts[0]}</li>
+        <li>{posts[1]}</li>
+        <li>{posts[2]}</li>
+        <li>{posts[3]}</li>
+        <li>{posts[4]}</li>
+        <li>{posts[5]}</li>
+      </div>
+     
     </div>
   );
 }
